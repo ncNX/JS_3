3813cdc812d86b42db563e144d3ebc461ad42f63
Igor <dumanschi.igor@gmail.com>
2024-10-04 20:00:13 +0300

    JS_3

---
+++ main.js
@@ -0,0 +1,34 @@
+// const swicthDrink = 'drink1'
+
+// switch (swicthDrink) {
+// 	case 'drink1': {
+// 		console.log('cola')
+// 		break;
+// 	}
+// 	case 'drink2': {
+// 		console.log('fanta')
+// 		break;
+// 	}
+// 	case 'drink3': {
+// 		console.log('ice tea')
+// 		break;
+// 	}
+// 	case 'drink4': {
+// 		console.log('water')
+// 		break;
+// 	}
+// 	default: {
+// 		console.log('alege bautura')
+// 	}
+// }
+
+const driver = {
+  name: "Alex",
+  age: "40",
+  city: "New York",
+  categori: "b1",
+};
+
+console.log(
+`Deiver's name is ${driver.name} , he's age is ${driver['age']} years`
+);
