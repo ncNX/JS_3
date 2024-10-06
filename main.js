583673cf062a41bbe413154ae938eac3a5cfae1b
Igor <dumanschi.igor@gmail.com>
2024-10-06 16:56:19 +0300

    JS_3

--- main.js
+++ main.js
@@ -1,34 +1,64 @@
-// const swicthDrink = 'drink1'
+// // const swicthDrink = 'drink1'
 
-// switch (swicthDrink) {
-// 	case 'drink1': {
-// 		console.log('cola')
-// 		break;
-// 	}
-// 	case 'drink2': {
-// 		console.log('fanta')
-// 		break;
-// 	}
-// 	case 'drink3': {
-// 		console.log('ice tea')
-// 		break;
-// 	}
-// 	case 'drink4': {
-// 		console.log('water')
-// 		break;
-// 	}
-// 	default: {
-// 		console.log('alege bautura')
-// 	}
-// }
+// // switch (swicthDrink) {
+// // 	case 'drink1': {
+// // 		console.log('cola')
+// // 		break;
+// // 	}
+// // 	case 'drink2': {
+// // 		console.log('fanta')
+// // 		break;
+// // 	}
+// // 	case 'drink3': {
+// // 		console.log('ice tea')
+// // 		break;
+// // 	}
+// // 	case 'drink4': {
+// // 		console.log('water')
+// // 		break;
+// // 	}
+// // 	default: {
+// // 		console.log('alege bautura')
+// // 	}
+// // }
 
-const driver = {
-  name: "Alex",
-  age: "40",
-  city: "New York",
-  categori: "b1",
-};
+// const driver = {
+//   name: "Alex",
+//   age: "40",
+//   city: "New York",
+//   categori: "b1",
+// };
 
-console.log(
-`Deiver's name is ${driver.name} , he's age is ${driver['age']} years`
-);
+// console.log(
+// `Deiver's name is ${driver.name} , he's age is ${driver['age']} years`
+// );
+
+
+const model = 'samsung';
+
+// console.log(model.length)
+
+// console.log(model.charAt(model.length - 1))
+// console.log(model[model.length - 1])
+
+// console.log(model.substring(2))
+
+// console.log(model.slice(3))
+
+// console.log(model.indexOf('m'))
+// console.log(model.lastIndexOf('u'))
+
+// console.log(model.replace('u', '+'))
+
+// console.log(model.split('s'))
+
+// console.log(model.toLowerCase())
+// console.log(model.toUpperCase())
+
+// const odin = 'Odin is the son of Borr and the jötunn Bestla';
+
+// console.log(odin.length)
+// console.log(odin[5])
+// console.log(odin.substring(7, 18))
+// console.log(odin.slice(-13))
+// console.log(odin.indexOf('son'))
